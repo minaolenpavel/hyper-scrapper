bec3e38a7e1b7ab0909480d9d73f61ca0979daa0
pavel <bellevillepaul03@gmail.com>
2024-04-20 23:24:06 +0200

    found search bar

--- selenium_test.py
+++ selenium_test.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+
 
 service = Service(executable_path=r"C:\Program Files (x86)\geckodriver.exe")
 options = webdriver.FirefoxOptions()
@@ -9,7 +12,13 @@ options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 driver = webdriver.Firefox(service=service, options=options)
 
 driver.get("https://planning.inalco.fr/public")
+search_bar = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit"))
+)
+search_bar.send_keys("russe")
 
-search_bar = driver.find_element(By.TAG_NAME, 'input')
-search_bar.send_keys("class1")
-search_bar.send_keys(Keys.RETURN)
+one = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "GInterface.Instances[1].Instances[1]_0"))
+)
+one.send_keys(Keys.ENTER)
+
