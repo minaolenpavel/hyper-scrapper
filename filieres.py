48f819b6aaa2484e4349aadc70fc2b46ed3b8a0f
Paul <bellevillepaul03@gmail.com>
2024-06-10 19:46:27 +0200

    now extracts ALL names of schedules in hyperplanning

--- filieres.py
+++ filieres.py
@@ -15,12 +15,32 @@ driver = webdriver.Firefox(service=service, options=options)
 driver.maximize_window()
 driver.get("https://planning.inalco.fr/public")
 
-search_bar = WebDriverWait(driver, 10).until(
-EC.presence_of_element_located((By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit")))
-search_bar.send_keys("Russe")
+
+
+search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit")))
+search_bar.send_keys(Keys.ENTER)
 time.sleep(3)
 results_list = driver.find_element(By.ID, "GInterface.Instances[1].Instances[1]_ContenuScroll")
+num_blocks = 33
+filieres_connues = []
 
-filieres = results_list.find_elements(By.TAG_NAME, "li")
-for filiere in filieres:
-    print(filiere.text)
+action = webdriver.ActionChains(driver)
+first_block = driver.find_element(By.ID, "GInterface.Instances[1].Instances[1]_bloc0")
+action.move_to_element(first_block)
+action.perform()
+
+for bloc in range(num_blocks+1):
+    if bloc != 33:
+        filiere = driver.find_element(By.ID, f"GInterface.Instances[1].Instances[1]_bloc{bloc}")
+        filiereplus = driver.find_element(By.ID, f"GInterface.Instances[1].Instances[1]_bloc{bloc+1}")
+        driver.execute_script("arguments[0].scrollIntoView();", filiereplus)
+        print(filiere.text)
+        filieres_connues.append(filiere.text)
+    elif bloc == 33:
+        filiere = driver.find_element(By.ID, f"GInterface.Instances[1].Instances[1]_bloc{bloc}")
+        print(filiere.text)
+        filieres_connues.append(filiere)
+    elif bloc == 34:
+        driver.quit
+        print(filieres_connues)
+
